7c37afdc4e23b596f2b2e577da7fcf27f357ebf5
Martínez Ortiz Saúl Axel <saulaxel.50@gmail.com>
2016-10-23 12:50:02 -0500

    iniciando la cración del doc

---
+++ tablas.sql
@@ -0,0 +1,9 @@
+--------------------------------------------
+-- Bases de Datos PROTECO                 --
+-- Proyecto 2: CreaciOn y manejo con SQL  --
+-- Integrantes:                           --
+--  * SAnchez Neri David Yaxkin           --
+--  * MartInez Ortiz SaUl Axel            --
+--------------------------------------------
+
+## CraciOn de las bases de datos
