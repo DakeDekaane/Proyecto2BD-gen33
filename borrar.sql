6bfb924d2375baf9dc0f63304c264d93d4caa679
Martínez Ortiz Saúl Axel <saulaxel.50@gmail.com>
2016-10-23 20:56:56 -0500

    Archivo de depurado

---
+++ borrar.sql
@@ -0,0 +1,51 @@
+--------------------------------------------
+-- Bases de Datos PROTECO                 --
+-- Proyecto 2: CreaciOn y manejo con SQL  --
+-- Integrantes:                           --
+--  * SAnchez Neri David Yaxkin           --
+--  * MartInez Ortiz SaUl Axel            --
+--------------------------------------------
+
+-- Borrado de tablas
+
+DROP TABLE Auto;
+
+DROP TABLE CatalogoModelos;
+
+DROP TABLE CatalogoMarcas;
+
+DROP TABLE Conductor;
+
+DROP TABLE CatalogoEstadosRepublica;
+
+DROP TABLE Cliente;
+
+DROP TABLE Tarjeta;
+
+DROP TABLE CatalogoTiposSeguro;
+
+DROP TABLE Cotizacion;
+
+DROP TABLE Poliza;
+
+DROP TABLE CatalogoAgencias;
+
+DROP TABLE AutoInvolucrado;
+
+DROP TABLE PersonaInvolucrada;
+
+DROP TABLE Hospital;
+
+DROP TABLE CatalogoEstadosSiniestro;
+
+DROP TABLE EstadoSiniestro;
+
+DROP TYPE SiniestroColision;
+
+DROP TYPE SiniestroSocial;
+
+DROP TYPE SiniestroMaterial;
+
+DROP TYPE Siniestro;
+
+DROP TABLE HistorialEstado;
