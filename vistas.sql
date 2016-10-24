efe8c0a04dc77b6a98ade8db1ebce0e7214ba4a4
DakeDekaane <servantarcherubw@gmail.com>
2016-10-24 08:32:30 -0500

    Se ponen los sinónimos en un script aparte

--- vistas.sql
+++ vistas.sql
@@ -6,29 +6,6 @@
 --  * MartInez Ortiz SaUl Axel            --
 --------------------------------------------
 
--- Creación de sinónimos
-CREATE SYNONYM Auto FOR Auto; -- Se me hace un poco contraproducente usar un sinónimo para esta tabla
-CREATE SYNONYM AutoInv FOR AutoInvolucrado;
-CREATE SYNONYM Agencias FOR CatalogoAgencias;
-CREATE SYNONYM TipoSin FOR CatalogoEstadosSiniestro;
-CREATE SYNONYM EstadoRep FOR CatalogoEstadosRepublica;
-CREATE SYNONYM Marca FOR CatalogoMarcas;
-CREATE SYNONYM Modelo FOR CatalogoModelos;
-CREATE SYNONYM TipoSeg FOR CatalogoTiposSeguro;
-CREATE SYNONYM Cte FOR Cliente;
-CREATE SYNONYM Cond FOR Conductor;
-CREATE SYNONYM Cot FOR Cotizacion;
-CREATE SYNONYM ESin FOR EstadoSiniestro;
-CREATE SYNONYM Historial FOR HistorialEstado;
-CREATE SYNONYM Hosp FOR Hospital;
-CREATE SYNONYM PersonaInv FOR PersonaInvolucrada;
-CREATE SYNONYM Plz FOR Poliza;
-CREATE SYNONYM Sin FOR Siniestro;
-CREATE SYNONYM SinColl FOR SiniestroColision;
-CREATE SYNONYM SinMat FOR SiniestroMaterial;
-CREATE SYNONYM SinSoc FOR SiniestroSocial;
-CREATE SYNONYM TarjetaCredito for Tarjeta;
-
 -- Vista de las tablas
 CREATE OR REPLACE VIEW Cotizaciones_Mayor_1900 AS
 	SELECT * FROM Cot WHERE Cot.costo > 1900;
